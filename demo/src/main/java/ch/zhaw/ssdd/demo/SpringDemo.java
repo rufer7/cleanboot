307eb3bec2a994fc4126bb583b8d5e15df9b7b4d
Marc Rufer <marc.rufer@isolutions.ch>
2026-03-02 17:38:32 +0100

    Add SpringDemo class

---
+++ demo/src/main/java/ch/zhaw/ssdd/demo/SpringDemo.java
@@ -0,0 +1,18 @@
+package ch.zhaw.ssdd.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemo.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
